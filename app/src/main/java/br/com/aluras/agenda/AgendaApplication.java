e6d3cc885928b1e5aa0e829b434d4e7efff3fa3d
Fabiano Nascimento <fabrodrigo@hotmail.com>
2022-10-29 13:12:56 -0300

    AgendaApplication: Especialização da Application para que esta fique responsável de executar uma única vez o código de criação de dados de teste

---
+++ app/src/main/java/br/com/aluras/agenda/AgendaApplication.java
@@ -0,0 +1,24 @@
+package br.com.aluras.agenda;
+
+import android.app.Application;
+
+import br.com.aluras.agenda.dao.AlunoDAO;
+import br.com.aluras.agenda.model.Aluno;
+
+public class AgendaApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        criaAlunosTeste();
+    }
+
+    private void criaAlunosTeste() {
+        AlunoDAO dao = new AlunoDAO();
+        for (int i = 2; i < 6; i++) {
+            dao.salva(new Aluno("José Nascimento " + String.valueOf(i), "(" + String.valueOf(i) + "3) 9999-1111", "fabrodrigo@hotmail.com"));
+            dao.salva(new Aluno("Maria Nascimento " + String.valueOf(i), "(" + String.valueOf(i) + "1) 8888-2222", "kassiusfab@hotmail.com"));
+            dao.salva(new Aluno("Nicolas Nascimento " + String.valueOf(i), "(" + String.valueOf(i) + "1) 8888-2222", "kassiusfab@hotmail.com"));
+        }
+    }
+}
